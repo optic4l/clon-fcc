bcba59cdcc72a65a4505354f217eb9daf032f556
lucas note <lksdro01@gmail.com>
2022-10-22 20:34:29 -0300

    primer avance

--- src/App.js
+++ src/App.js
@@ -4,9 +4,11 @@ import Testimonio from './components/Testimonio.js';
 
 function App() {
   return (
-    <div className="App">
-      <Testimonio />
-      
+    <div className='App'>
+      <div className='contenedor-principal'>
+        <h1>Esto es lo que dicen los alumnos sobre freecodeCamp</h1>
+        <Testimonio />
+      </div> 
     </div>
   );
 }
